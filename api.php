3ffc61041d5ea4d5666fa48230c180c9f013bdf0
angallou <angallou@ensc.fr>
2025-04-24 11:21:43 +0200

    Ajout API

---
+++ api.php
@@ -0,0 +1,39 @@
+<?php 
+
+function envoiJSON($tab){
+    header('Content-Type: application/json');
+    //print_r($tab);
+    $json = json_encode($tab, JSON_UNESCAPED_UNICODE);
+    echo $json;
+}
+
+function recupVilles($texte) {
+    //variables de connexion
+    $host = 'localhost';		
+    $dbname = 'vcollege';
+    $username = 'root';
+    $password = '';
+    //tentative de connexion à la base de donnée
+    try {
+        $bdd = new PDO('mysql:host='. $host .';dbname='. $dbname .';charset=utf8',
+        $username, $password);
+
+    } catch(Exception $e) {
+        // Si erreur, tout arrêter
+        die('Erreur : '. $e->getMessage());
+    }
+    //préparation de la requête
+    $requete = "select * from note";
+    //requête auprès de la base
+    $resultat = $bdd->query($requete);
+    // On récupère tout dans la variable tableau
+    $tableau = $resultat->fetchall(); 
+    return $tableau; 
+    //affichage du tableau - c'est un tableau de tableau
+    //print_r($tableau); 
+    // foreach($tableau as $cellule){
+    //     echo $cellule['ville_nom']."   ".$cellule['ville_code_postal']."<br>";
+    // } 
+}
+
+?>  
